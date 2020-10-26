daa28d0d56dfdf56b30a3d567cd2ef7d8717797a
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-10-25 19:09:23 -0700

    created color model

---
+++ HW4/HW4Solution/HW4Project/Models/RGBColorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW4Project.Models
+{
+    public class RGBColorModel
+    {
+        public int? Red { get; set; }
+        public int? Green { get; set; }
+        public int? Blue { get; set; }
+    }
+}

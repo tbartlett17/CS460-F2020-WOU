04459c4ebca3b83003a9dffc7e5caa86ed57f0f2
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-10-25 21:06:10 -0700

    creation of color interpolation model

---
+++ HW4/HW4Solution/HW4Project/Models/ColorInterpolation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW4Project.Models
+{
+    public class ColorInterpolation
+    {
+        public string FirstColor { get; set; }
+        public string SecondColor { get; set; }
+        public int? NumberOfColors { get; set; }
+        public List<string> ColorList { get; set; }
+
+        public override string ToString()
+        {
+            return $"firstColor={FirstColor}, secondColor={SecondColor}, NumColors={NumberOfColors}"; ;
+        }
+    }
+}

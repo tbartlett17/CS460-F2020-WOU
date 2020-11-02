2cf55e8c65595feacc2a19f83745b82c78e0188c
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-11-01 22:14:11 -0800

    initial upload with sql scripts

---
+++ HW5/HW5Solution/HW5Project/Data/seed.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO [HomeworkAssignments] (Priority, DueDate, TimeDue, Department, CourseNumber, Assignment, Notes) VALUES
+	('High', '2019-11-05', '22:00:00', 'CS', '460', 'HW5 Assignment', 'MVC Application with simple database. 
+		Instructions availalbe at https://wou-cs46x-resources.netlify.com/cs460/hw/hw5'),
+	('Low', '2019-11-05', '12:00:00', 'LIT', '102', 'Wk 6 Discussion Questions p1', 'Intro to Drama, assignment done through moodle.'),
+	('Low', '2019-11-06', '12:00:00', 'LIT', '102', 'Wk 6 Discussion Questions p2', ''),
+	('Low', '2019-11-07', '12:00:00', 'LIT', '102', 'Wk 6 Discussion Questions p3', ''),
+	('Medium', '2019-11-30', '09:00:00', 'PE', '188', 'Goals Paper', 'Reflection Paper on what i want to improve about myself.
+		Instructions avaialbe via class sylabus.')
---
+++ HW5/HW5Solution/HW5Project/Data/up.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [HomeworkAssignments]
+(
+    [ID]			INT PRIMARY KEY IDENTITY (1,1),
+    [Priority]		NVARCHAR(6)			NOT NULL,
+    [DueDate]	    DATE				NOT NULL,
+    [TimeDue]		DATETIME            NOT NULL,
+	[Department]	NVARCHAR(4)			NOT NULL,
+	[CourseNumber]	INT					NOT NULL,
+	[Assignment]	NVARCHAR(64)		NOT NULL,
+	[Notes]			NVARCHAR(MAX)
+)
+GO

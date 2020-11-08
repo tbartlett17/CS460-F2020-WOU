373ff1259cfa6fa56b976aa72371fb84e2957454
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-11-08 08:41:39 -0800

    push file

--- HW5/HW5Solution/HW5Project/Models/HomeworkAssignment.cs
+++ HW5/HW5Solution/HW5Project/Models/HomeworkAssignment.cs
@@ -7,5 +7,13 @@ namespace HW5Project.Models
 {
     public class HomeworkAssignment
     {
+        public int ID { get; set; }
+        public int Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime TimeDue { get; set; }
+        public string Department { get; set; }
+        public int CourseNumber { get; set; }
+        public string Assignment { get; set; }
+        public string Notes { get; set; }
     }
 }

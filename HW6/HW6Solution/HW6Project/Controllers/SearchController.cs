04a538daceee2cbb2325035af811348bf73a137d
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-11-08 18:17:51 -0800

    first implementation of working display of artist page

---
+++ HW6/HW6Solution/HW6Project/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using HW6Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace HW6Project.Controllers
+{
+    public class SearchController : Controller
+    {
+        private HW6DBContext db;
+
+        public SearchController(HW6DBContext db)
+        {
+            this.db = db;
+        }
+        
+           
+
+        [HttpGet]
+        public IActionResult Artist(int? id)
+        {
+            Debug.WriteLine("inside /Search/Artist action method");
+
+            return View(db.Artists.Include(a => a.Albums).ThenInclude(t => t.Tracks).Where(a => a.ArtistId == 51));
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

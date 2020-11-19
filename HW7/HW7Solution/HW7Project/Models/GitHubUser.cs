518b75ca57c6dde532d995188884ac81916eb4bd
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-11-18 22:07:29 -0800

    data models for json returned from GitHib API

---
+++ HW7/HW7Solution/HW7Project/Models/GitHubUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW7Project.Models
+{
+    public class GitHubUser
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+
+        public string Avatar_url { get; set; }
+        public IEnumerable<GitHubRepo> Repos { get; set; }
+
+    }
+}

cc01e71631ccb85ef1bab44c7312619a96f41617
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-11-19 16:42:04 -0800

    cleaner implementation of the table and ajax, now with remove old commits before adding more

--- HW7/HW7Solution/HW7Project/wwwroot/js/site.js
+++ HW7/HW7Solution/HW7Project/wwwroot/js/site.js
@@ -5,11 +5,18 @@
 
 
 $("input").click(function (e) {
+
+    var table = document.getElementById("commitsTable");
+    while (table.rows.length > 0) {
+        table.deleteRow(0);
+    }
+
     var idClicked = e.target.id;
     //console.log(idClicked);
 
     let address = "/api/commits?" + idClicked;
     console.log(address);
+
 
     $.ajax({
         type: "GET",
@@ -29,19 +36,32 @@ function displayCommits(data) {
     console.log(data);
 
     var table = document.getElementById("commitsTable");
+    var header = table.createTHead();
+    var body = table.createTBody();
+
+    var headRow = header.insertRow(0);
+    var hcell1 = headRow.insertCell(); //SHA
+    var hcell2 = headRow.insertCell(); //Timestamp
+    var hcell3 = headRow.insertCell(); //Committer
+    var hcell4 = headRow.insertCell(); //Commit Message
+    hcell1.innerHTML = "SHA".bold();
+    hcell2.innerHTML = "Timestamp".bold(); 
+    hcell3.innerHTML = "Committer".bold();
+    hcell4.innerHTML = "Commit Message".bold();
+
 
     for (var i = 0, d; d = data[i]; i++) {
 
-        var row = table.insertRow(i + 1);
-        var cell1 = row.insertCell(); //SHA
-        var cell2 = row.insertCell(); //Timestamp
-        var cell3 = row.insertCell(); //Committer
-        var cell4 = row.insertCell(); //Commit Message
+        var bodyRow = body.insertRow(0);
+        var bcell1 = bodyRow.insertCell(); //SHA
+        var bcell2 = bodyRow.insertCell(); //Timestamp
+        var bcell3 = bodyRow.insertCell(); //Committer
+        var bcell4 = bodyRow.insertCell(); //Commit Message
 
-        cell1.innerHTML = d.sha;
-        cell2.innerHTML = d.commit.author.date;
-        cell3.innerHTML = d.commit.author.name;
-        cell4.innerHTML = d.commit.message;
+        bcell1.innerHTML = d.sha;
+        bcell2.innerHTML = d.commit.author.date;
+        bcell3.innerHTML = d.commit.author.name;
+        bcell4.innerHTML = d.commit.message;
     }
 
 

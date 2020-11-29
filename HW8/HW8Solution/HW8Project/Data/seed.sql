e65d3661c327019c0c0a1e58f55e499ba882e546
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-11-29 15:12:22 -0800

    initial HW8 created

---
+++ HW8/HW8Solution/HW8Project/Data/seed.sql
@@ -0,0 +1,21 @@
+INSERT INTO [Courses] (course_id, title, description) VALUES
+    ('CS460', 'Software Development', ''),
+    ('LIT102', '', ''),
+    ('MTH341')
+
+INSERT INTO [Keywords] (title)
+    ('coding'),
+    ('ajax'),
+    ('database'),
+    ('video'),
+    ('hard'),
+    ('easy'),
+    ('time consuming'),
+    ('mvc'),
+    ('sql'),
+    ('azure')
+
+INSERT INTO [Assignments] (priority, due_date, course_id, title, keywords, notes)
+    ('High', '2020-11-29', 'CS460', 'HW8 Assignment', '5,7', 'hw9 utilizes this'),
+    ('Medium', '2020-12-04', 'CS460', 'HW9 Assignment', '10', ''),
+    ('Low', '2020-12-01', 'LIT102', 'Book Report', '', '')
---
+++ HW8/HW8Solution/HW8Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW8Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

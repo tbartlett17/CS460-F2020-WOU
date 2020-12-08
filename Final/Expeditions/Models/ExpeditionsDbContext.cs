606ce8ff6225dbadb63a0bc0419858230b93e2e0
Tyler Bartlett <tbartlett17@mail.wou.edu>
2020-12-07 19:07:47 -0800

    scaffolded using dotnet ef dbcontext...

---
+++ Final/Expeditions/Models/ExpeditionsDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Expeditions.Models
+{
+    public partial class ExpeditionsDbContext : DbContext
+    {
+        public ExpeditionsDbContext()
+        {
+        }
+
+        public ExpeditionsDbContext(DbContextOptions<ExpeditionsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Expedition> Expeditions { get; set; }
+        public virtual DbSet<Peak> Peaks { get; set; }
+        public virtual DbSet<TrekkingAgency> TrekkingAgencies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=Expeditions");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Expedition>(entity =>
+            {
+                entity.HasOne(d => d.Peak)
+                    .WithMany(p => p.Expeditions)
+                    .HasForeignKey(d => d.PeakId)
+                    .HasConstraintName("Expedition_FK_Peak");
+
+                entity.HasOne(d => d.TrekkingAgency)
+                    .WithMany(p => p.Expeditions)
+                    .HasForeignKey(d => d.TrekkingAgencyId)
+                    .HasConstraintName("Expedition_FK_TrekkingAgency");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Final/Expeditions/Models/TrekkingAgency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expeditions.Models
+{
+    [Table("TrekkingAgency")]
+    public partial class TrekkingAgency
+    {
+        public TrekkingAgency()
+        {
+            Expeditions = new HashSet<Expedition>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [InverseProperty(nameof(Expedition.TrekkingAgency))]
+        public virtual ICollection<Expedition> Expeditions { get; set; }
+    }
+}
